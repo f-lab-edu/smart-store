9a90fd8917cb5d4989a60cdc136495d5cee2bb90
hitman1439 <hitman1439@gmail.com>
2021-03-09 23:49:44 +0900

    [#3] 불필요 주석 삭제

--- src/main/java/com/project/smartStore/dto/UserDto.java
+++ src/main/java/com/project/smartStore/dto/UserDto.java
@@ -4,11 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-/**
- *
- * @Builder :
- *
- */
+
 @Getter
 @Setter
 @AllArgsConstructor
