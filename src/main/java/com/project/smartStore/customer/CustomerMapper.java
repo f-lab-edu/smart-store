7294bd3de524be9eb874f69fd02bd4902fb9dafa
sunho-lee <sh112123@naver.com>
2021-03-13 15:59:18 +0900

    마이바티스 매퍼 인터페이스 추가

---
+++ src/main/java/com/project/smartStore/customer/CustomerMapper.java
@@ -0,0 +1,17 @@
+package com.project.smartStore.customer;
+
+import java.sql.SQLIntegrityConstraintViolationException;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface CustomerMapper {
+
+  CustomerDTO selectCustomerDetailById(String id);
+  void insertCustomer(CustomerDTO params) throws SQLIntegrityConstraintViolationException;
+  int updateCustomerPwd(String id, String updatePwd);
+  int updateCustomerName(String id, String updateName);
+  int updateCustomerPhoneNum(String id, String updatePhoneNum);
+  int deleteCustomerById(String id);
+
+
+}
