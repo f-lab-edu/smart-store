dd6d15b6480f63271b4646de91ed4bfbea7bb474
sunho-lee <sh112123@naver.com>
2021-03-22 20:11:50 +0900

    DI 구현 방식 변경
    생성자를 직접 구현한 것에서 lombok 애노테이션 방식으로 변경

--- src/main/java/com/project/smartStore/customer/CustomerController.java
+++ src/main/java/com/project/smartStore/customer/CustomerController.java
@@ -1,5 +1,6 @@
 package com.project.smartStore.customer;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -10,14 +11,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequiredArgsConstructor
 @RequestMapping("/customer")
 public class CustomerController {
 
   private final CustomerService customerService;
-
-  public CustomerController(CustomerService customerService) {
-    this.customerService = customerService;
-  }
 
   @PostMapping
   public boolean registerCustomer(@RequestBody CustomerDTO customerDTO){
