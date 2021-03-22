be72a7b2bb84bd29ba0ab5ac552f8b261a01ed4a
sunho-lee <sh112123@naver.com>
2021-03-22 21:47:58 +0900

    필요없는 lombok 애노테이션 삭제
    @setter를 삭제하여 객체의 무분별한 수정을 방지
    사용하지 않는 @ToString을 삭제

--- src/main/java/com/project/smartStore/customer/CustomerDTO.java
+++ src/main/java/com/project/smartStore/customer/CustomerDTO.java
@@ -3,14 +3,10 @@ package com.project.smartStore.customer;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
-import lombok.ToString;
 
 @AllArgsConstructor
 @NoArgsConstructor
 @Getter
-@Setter
-@ToString
 public class CustomerDTO {
 
   private String id;
