d0d0ad19137f02f264b091e80a3b0109b195411f
isaac <hitman1439@naver.com>
2021-05-07 14:59:16 +0900

    [#8] 가독성을 위한 개행 추가

--- src/main/java/com/project/smartstore/exception/UnAuthenticatedAccessException.java
+++ src/main/java/com/project/smartstore/exception/UnAuthenticatedAccessException.java
@@ -1,7 +1,9 @@
 package com.project.smartstore.exception;
 
 public class UnAuthenticatedAccessException extends RuntimeException{
+  
   public UnAuthenticatedAccessException() {}
+  
   public UnAuthenticatedAccessException(String message) {
     super(message);
   }
