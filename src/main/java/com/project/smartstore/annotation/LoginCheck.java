ba410c9da29be696453cc29cdbf3597ef9ed381b
hitman1439 <hitman1439@gmail.com>
2021-05-09 18:05:25 +0900

    [#8] 커스텀 어노테이션 설정에 대한 설명 추가

--- src/main/java/com/project/smartstore/annotation/LoginCheck.java
+++ src/main/java/com/project/smartstore/annotation/LoginCheck.java
@@ -7,6 +7,13 @@ import java.lang.annotation.Target;
 
 /**
  * 로그인 체크를 위한 어노테이션.
+ *
+ * @Retention
+ *      : 어노테이션 유형이있는 어노테이션이 보존되는 기간을 나타낸다.
+ *      어노테이션 유형 선언에 Retention 어노테이션이없는 경우 기본값은 RetentionPolicy.CLASS이다.
+ * @Target
+ *      : 어노테이션 선언할 타입을 지정한다.
+ *
  */
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.METHOD)
