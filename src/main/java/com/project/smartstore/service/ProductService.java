affbff21506a3c18ea4c310f1e3d5d4c2371ed72
sunho-lee <sh112123@naver.com>
2021-06-01 00:55:40 +0900

    리팩토링 : ProductService 인터페이스 삭제
    ProductService인터페이스를 삭제하고 구현체인 ProductServiceimpl를 ProductService로 변경하여 필요없는 service, serviceimpl를 리팩토링하였습니다.

--- src/main/java/com/project/smartstore/service/ProductService.java
+++ src/main/java/com/project/smartstore/service/ProductService.java
@@ -1,10 +1,17 @@
 package com.project.smartstore.service;
 
 import com.project.smartstore.dto.ProductDto;
+import com.project.smartstore.mapper.ProductMapper;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
 
+@Service
+@RequiredArgsConstructor
+public class ProductService {
 
-public interface ProductService {
+  private final ProductMapper productMapper;
 
-  void addProductInStore(ProductDto productDto);
-
+  public void addProductInStore(ProductDto productDto) {
+    productMapper.insertProduct(productDto);
+  }
 }
