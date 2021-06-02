d766780481d59accab1468e6f870ef8b76b19f92
sunho-lee <sh112123@naver.com>
2021-06-02 22:32:42 +0900

    리팩토링 : ProductListDto 누락된 것 추가

---
+++ src/main/java/com/project/smartstore/dto/ProductListDto.java
@@ -0,0 +1,20 @@
+package com.project.smartstore.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class ProductListDto {
+
+  private int productId;
+  private String productName;
+  private String categoryId;
+  private int price;
+  private String mainImg;
+
+}
