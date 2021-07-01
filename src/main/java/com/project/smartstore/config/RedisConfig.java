95775c99f0b45a4e4db638bf84cb26c3a001c89c
sunho-lee <sh112123@naver.com>
2021-07-01 14:17:04 +0900

    기능 : 레디스 default 설정 누락된 것 추가

--- src/main/java/com/project/smartstore/config/RedisConfig.java
+++ src/main/java/com/project/smartstore/config/RedisConfig.java
@@ -72,6 +72,7 @@ public class RedisConfig {
     return RedisCacheManager.RedisCacheManagerBuilder
         .fromConnectionFactory(redisConnectionFactory)
         .withInitialCacheConfigurations(cacheConfigurationMap)
+        .cacheDefaults(defaultRedisCacheConfiguration())
         .build();
   }
 
